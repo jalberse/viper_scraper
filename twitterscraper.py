d5870110e01ed2a45a217f5b0151bff6e8666a9f
John Alberse <alberse.john@gmail.com>
2019-01-25 10:55:20 -0600

    set up OAuth and tweepy API

--- twitterscraper.py
+++ twitterscraper.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 import os.path
 
 # set up OAuth
@@ -10,3 +11,16 @@ try:
         keys_file = open("keys",'r')
 except OSError as e:
     print("Error opening keys file")
+
+keys = json.load(keys_file)
+
+consumer_key = keys['websites']['Twitter']['consumer_key']
+consumer_secret = keys['websites']['Twitter']['consumer_secret']
+access_token = keys['websites']['Twitter']['access_token']
+access_token_secret = keys['websites']['Twitter']['access_secret']
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
