0d18e0726af330659405194d1ae5b0248a741d69
John Alberse <alberse.john@gmail.com>
2019-01-31 14:54:00 -0600

    small changes

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -5,12 +5,12 @@ import os.path
 ## set up OAuth from the keys file. 
 ## .my_keys (in .gitignore) file takes precedence over the keys file for easily maintaining private keys
 try:
-    if (os.path.isfile(".my_keys")):
-        keys_file = open(".my_keys",'r')
+    if (os.path.isfile("../twitter/.my_keys")):
+        keys_file = open("../twitter/.my_keys",'r')
     else: 
-        keys_file = open("keys.json",'r')
+        keys_file = open("../twitter/keys.json",'r')
 except OSError as e:
-    print("Error opening keys.json")
+    print("Error opening keys file")
 
 keys = json.load(keys_file)
 
@@ -23,3 +23,6 @@ auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
+
+def scrape(seed_user,images=True,text=False,number=1000,limit_per_node=20):
+    print('test')
--- vscraper.py
+++ vscraper.py
@@ -1,21 +1,28 @@
 import argparse
 import string
-import twitter.scraper
+import sys
+
+import twitter
 
 parser = argparse.ArgumentParser(description="Scrape data from social media")
 
 parser.add_argument('website',choices=['twitter','instagram'],
     nargs=1,
     help='The website to crawl for data')
-parser.add_argument('seed user',nargs=1,
+parser.add_argument('seed_user',nargs=1,
     help='The seed user handle, e.g. @Twitter')
-parser.add_argument('-t','--type',choices=['images','text','all'],
+parser.add_argument('-dt','--data_type',choices=['images','text','all'],
     nargs=1,default='images',dest='data_type',
     help="The data type to collect. Images only by default. 'all' is images + text")
 parser.add_argument('-n','--number',type=int,nargs=1,default=1000,
-    dest='num',metavar='Number',
+    dest='number',metavar='Number',
     help="If data type is images, the number of images to scrape. Else the number of posts to scrape.")
 parser.add_argument('-l','--limit',nargs=1,metavar="Per-Node-Limit",
     type=int,dest='limit_per_node',default=20,
     help="The number of posts/images to extract per node before moving to neighbor")
 args = parser.parse_args()
+
+if (args.website=='twitter'):
+    if (args.data_type=='images'):
+        twitter.scraper.scrape(args.seed_user,images=True,text=False,number=args.number,
+            limit_per_node=args.limit_per_node)
