778d9e246c2a0cb6fd8a4404b43cb9c8ac6d914f
John Alberse <alberse.john@gmail.com>
2019-02-01 15:46:17 -0600

    Fix arg parsing (was extracting lists before)

--- viper_scraper/vscraper.py
+++ viper_scraper/vscraper.py
@@ -7,17 +7,16 @@ from twitter import scraper as tscraper
 parser = argparse.ArgumentParser(description="Scrape data from social media")
 
 parser.add_argument('website',choices=['twitter','instagram'],
-    nargs=1,
     help='The website to crawl for data')
-parser.add_argument('seed_user',nargs=1,
+parser.add_argument('seed_user',
     help='The seed user handle, e.g. @Twitter')
-parser.add_argument('-n','--number',type=int,nargs=1,default=1000,
+parser.add_argument('-n','--number',type=int,default=1000,
     dest='number',metavar='Number',
     help="If data type is images, the number of images to scrape. Else the number of posts to scrape.")
-parser.add_argument('-l','--limit',nargs=1,metavar="Per-Node-Limit",
+parser.add_argument('-l','--limit',metavar="Per-Node-Limit",
     type=int,dest='limit_per_node',default=20,
     help="The number of posts/images to extract per node before moving to neighbor")
 args = parser.parse_args()
 
-if (args.website[0]=='twitter'): 
-    tscraper.test()
+if (args.website=='twitter'): 
+    tscraper.scrape(seed_user=args.seed_user,number=args.number,limit_per_node=args.limit_per_node)
