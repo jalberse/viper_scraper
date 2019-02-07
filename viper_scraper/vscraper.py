5e09e6d9a5758ac13fd8d68ae0966a0ef08b87c9
John Alberse <alberse.john@gmail.com>
2019-02-07 15:39:53 -0600

    Fix SS to actually limit images per node, and some minor clarifications in argparser

--- viper_scraper/vscraper.py
+++ viper_scraper/vscraper.py
@@ -4,7 +4,14 @@ import sys
 
 from twitter import scraper as tscraper
 
-parser = argparse.ArgumentParser(description="Scrape data from social media")
+DEFAULT_NUMBER = 1000
+DEFAULT_PER_NODE_LIMIT = 20
+DEFAULT_FOLLOWER_LIMIT = -1
+
+parser = argparse.ArgumentParser(description="Scrape data from social media",
+                                epilog="For snowball sampling, reducing per-node-limit and followers-per-node will " +
+                                "reduce locality of data collected (ie increase depth of the graph traversed " +
+                                "at the cost of increased processing time to due Twitter API rate limits.")
 
 parser.add_argument('website',choices=['twitter','instagram'],
     help='The website to crawl for data')
@@ -17,12 +24,12 @@ parser.add_argument('-nl','--node_limit',metavar="Per-Node-Limit",
     type=int,dest='limit_per_node',default=20,
     help="The number of posts/images to extract per node before moving to neighbor. Note: Decreasing "
             " this number increases the time it takes to run, due to twitter rate limits.")
-parser.add_argument('-fl','--follower_limit',metavar="Neighbors-per-node",
-    type=int,dest='limit_neighbors_per_node',default=-1,
-    help="The number of neigbors to visit per node. Visit all neighbors if not listed.")
+parser.add_argument('-fl','--follower_limit',metavar="Followers-Per-Node",
+    type=int,dest='limit_followers_per_node',default=-1,
+    help="For snowball sampling, the number of neigbors to visit per node. Visit all neighbors if not listed.")
 args = parser.parse_args()
 
 if (args.website=='twitter'): 
     tscraper.snowball_scrape(seed_user_screen_name=args.seed_user,number=args.number,
                     limit_per_user=args.limit_per_node,
-                    limit_neighbors_per_node=args.limit_neighbors_per_node)
+                    limit_neighbors_per_node=args.limit_followers_per_node)
