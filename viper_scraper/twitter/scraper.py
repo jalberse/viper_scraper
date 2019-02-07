40fcfb9b8bafaf0b558409a1a3a65d34853c8d68
John Alberse <alberse.john@gmail.com>
2019-02-07 14:32:28 -0600

    Finish snowball sampling scraping

--- viper_scraper/twitter/scraper.py
+++ viper_scraper/twitter/scraper.py
@@ -1,7 +1,9 @@
 import tweepy
 import json
+import urllib.request
 import os.path
 import queue
+import uuid
 
 DEBUG = 1
 
@@ -26,13 +28,14 @@ access_token_secret = keys['websites']['Twitter']['access_secret']
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 
-api = tweepy.API(auth_handler=auth,wait_on_rate_limit=True)
+api = tweepy.API(auth_handler=auth,wait_on_rate_limit=True,
+                wait_on_rate_limit_notify=True)
 
 
-def scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_per_node=-1):
+def snowball_scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_per_node=-1):
     """ 
     Scrape twitter for images. 
-    Uses breadth-first-search to extract limit_per_node images
+    Uses snowball sampling to extract limit_per_node images
     from each user, visiting limit_num_neighbors per user
     until number images have been downloaded
 
@@ -44,7 +47,7 @@ def scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_p
     """
 
     # Set up where to save data
-    data_dir = ("../data/")
+    data_dir = ("data/")
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
     
@@ -63,12 +66,12 @@ def scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_p
     while ((to_visit.empty() is not True) and (k > 0)):
         user_id = to_visit.get()
         # Scrape this user and update number of images left
+        if (DEBUG): print("Visiting: " + str(user_id))
+
         k = k - scrape_user(user_id=user_id,limit=limit_per_user,data_dir=data_dir)
 
-        if (DEBUG):
-            print("Visiting: " + str(user_id))
-
         # Gather a list of follower/neighbor IDs
+        if (DEBUG): print("Getting neighbors of " + str(user_id))
         follower_ids = []
         for follower_id in tweepy.Cursor(api.followers_ids, id=user_id).items():
             # Check if reached max neighbors to search, if applicable
@@ -86,11 +89,40 @@ def scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_p
 
 def scrape_user(user_id,limit,data_dir):
     """
-    Scrape a single user for images.
-    Returns the number of images scraped
+    Scrape a single user for images. Gets all images.
+    Returns the number of images scraped.
 
     user -- the twitter user object to scrape
     limit -- the maximum number of images to scrape from this user. If -1, no limit
     data_dir -- the directory to save images to
     """
-    return 100 # TODO just testing
+    if (DEBUG): print("Scraping user " + str(user_id))
+
+    # Get all tweets from user
+    tweets = api.user_timeline(id=user_id,count=200)
+    last_id = tweets[-1].id
+    while (True):
+        tweets_to_append = api.user_timeline(id=user_id,count=200,
+                                        max_id=last_id-1)
+        if (len(tweets_to_append)==0):
+            break
+        else:
+            last_id = tweets_to_append[-1].id-1
+            tweets = tweets + tweets_to_append
+    
+    # Collect a set of image URLs from the user
+    media_files = set()
+    for status in tweets:
+        media = status.entities.get('media',[])
+        if (len(media) > 0): # each status may have multiple
+            for i in range (0, len(media)):
+                if (media[i]['type'] == 'photo'):
+                    media_files.add(media[i]['media_url'])
+
+    # Download the images
+    n = 0 # num of images downloaded from user
+    for media_file in media_files:
+        urllib.request.urlretrieve(media_file,
+            os.path.join(data_dir,str(uuid.uuid4().hex) + ".jpg"))
+        n = n + 1
+    return n
