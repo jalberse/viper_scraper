7270b4f8cb9185238a20cb4642d9652f5658c51b
John Alberse <alberse.john@gmail.com>
2019-02-07 15:41:05 -0600

    Actually use defaults for argparse

--- viper_scraper/vscraper.py
+++ viper_scraper/vscraper.py
@@ -17,15 +17,15 @@ parser.add_argument('website',choices=['twitter','instagram'],
     help='The website to crawl for data')
 parser.add_argument('seed_user',
     help='The seed user handle, e.g. @Twitter')
-parser.add_argument('-n','--number',type=int,default=1000,
+parser.add_argument('-n','--number',type=int,default=DEFAULT_NUMBER,
     dest='number',metavar='Number',
     help="If data type is images, the number of images to scrape. Else the number of posts to scrape.")
 parser.add_argument('-nl','--node_limit',metavar="Per-Node-Limit",
-    type=int,dest='limit_per_node',default=20,
+    type=int,dest='limit_per_node',default=DEFAULT_PER_NODE_LIMIT,
     help="The number of posts/images to extract per node before moving to neighbor. Note: Decreasing "
             " this number increases the time it takes to run, due to twitter rate limits.")
 parser.add_argument('-fl','--follower_limit',metavar="Followers-Per-Node",
-    type=int,dest='limit_followers_per_node',default=-1,
+    type=int,dest='limit_followers_per_node',default=DEFAULT_FOLLOWER_LIMIT,
     help="For snowball sampling, the number of neigbors to visit per node. Visit all neighbors if not listed.")
 args = parser.parse_args()
 
