b36939552abec965e5f87ca11e1c402d45d6c1dd
John Alberse <alberse.john@gmail.com>
2019-02-12 12:25:43 -0600

    fix linting issues

--- viper_scraper/twitter/scraper.py
+++ viper_scraper/twitter/scraper.py
@@ -8,34 +8,38 @@ from random import randint
 
 DEBUG = 1
 
-## set up OAuth from the keys file. 
+## set up OAuth from the keys file.
 ## .my_keys (in .gitignore) file takes precedence over the keys file for easily maintaining private keys
-try:
-    ## TODO: Fix paths for best practice compliance
-    if (os.path.isfile(".my_keys")):
-        keys_file = open(".my_keys",'r')
-    else: 
-        keys_file = open("metadata/keys.json",'r')
-except OSError as e:
-    print("Error opening keys file")
 
-keys = json.load(keys_file)
+def get_api():
+    """
+    Handles OAuth and returns the tweepy API
+    """
+    try:
+        ## TODO: Fix paths for best practice compliance
+        if os.path.isfile(".my_keys"):
+            keys_file = open(".my_keys", 'r')
+        else:
+            keys_file = open("metadata/keys.json", 'r')
+    except OSError:
+        print("Error opening keys file")
 
-consumer_key = keys['websites']['Twitter']['consumer_key']
-consumer_secret = keys['websites']['Twitter']['consumer_secret']
-access_token = keys['websites']['Twitter']['access_token']
-access_token_secret = keys['websites']['Twitter']['access_secret']
+    keys = json.load(keys_file)
 
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+    consumer_key = keys['websites']['Twitter']['consumer_key']
+    consumer_secret = keys['websites']['Twitter']['consumer_secret']
+    access_token = keys['websites']['Twitter']['access_token']
+    access_token_secret = keys['websites']['Twitter']['access_secret']
 
-api = tweepy.API(auth_handler=auth,wait_on_rate_limit=True,
-                wait_on_rate_limit_notify=True)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
+    return tweepy.API(auth_handler=auth, wait_on_rate_limit=True,
+                      wait_on_rate_limit_notify=True)
 
-def snowball_scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_neighbors_per_node=-1):
-    """ 
-    Scrape twitter for images. 
+def snowball_scrape(seed_user_screen_name, number=1000, limit_per_user=-1, limit_neighbors_per_node=-1):
+    """
+    Scrape twitter for images.
     Uses snowball sampling to extract limit_per_node images
     from each user, visiting limit_num_neighbors per user
     until number images have been downloaded.
@@ -46,12 +50,13 @@ def snowball_scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_ne
     limt_per_node -- the number of images to scrape per user
     limit_num_neighbors -- The maximum number of neighbors visited per user
     """
+    api = get_api()
 
     # Set up where to save data
     data_dir = ("data/")
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
-    
+
     k = number # number of images remaining to scrape
 
     # Queue and set of IDs (not user objects)
@@ -59,46 +64,52 @@ def snowball_scrape(seed_user_screen_name,number=1000,limit_per_user=-1,limit_ne
     to_visit = queue.Queue()
     visited = set()
 
-    user = api.get_user(seed_user_screen_name)    
+    user = api.get_user(seed_user_screen_name)
     to_visit.put(user.id)
     visited.add(user.id)
 
     while ((k > 0) and (to_visit.empty() is not True)):
         user_id = to_visit.get()
         # Scrape this user and update number of images left
-        if (DEBUG): print("Visiting: " + str(user_id))
+        if DEBUG:
+            print("Visiting: " + str(user_id))
 
-        k = k - scrape_user_images(user_id=user_id,limit=limit_per_user,data_dir=data_dir)
+        k = k - scrape_user_images(user_id=user_id, limit=limit_per_user,
+                                   data_dir=data_dir,
+                                   api=api)
 
         # Mark new nodes to visit iff to_visit queue is sufficiently small
         # This reduces GET followers calls, which are severely rate limited
-        if (to_visit.qsize() < 10):
+        if to_visit.qsize() < 10:
             # Gather a list of follower/neighbor IDs
-            if (DEBUG): print("Queue low, getting neighbors of " + str(user_id))
+            if DEBUG:
+                print("Queue low, getting neighbors of " + str(user_id))
             follower_ids = []
             try:
                 # TODO: Can we avoid GET followers while queue is of sufficient length?
                 # May speed up scraping considerably.
                 for follower_id in tweepy.Cursor(api.followers_ids, id=user_id).items():
                     # Check if reached max neighbors to search, if applicable
-                    if (len(follower_ids) == limit_neighbors_per_node): break
+                    if len(follower_ids == limit_neighbors_per_node):
+                        break
                     follower_ids.append(follower_id)
             except tweepy.error.TweepError:
-                if (DEBUG): print("Private/suspended/deleted user " + str(user_id) + ", skipping")
+                if DEBUG:
+                    print("Private/suspended/deleted user " + str(user_id) + ", skipping")
 
             # BFS
             for u_id in follower_ids:
-                if (u_id not in visited):
+                if u_id not in visited:
                     to_visit.put(u_id)
                     visited.add(u_id)
-    
-    if (DEBUG):
+
+    if DEBUG:
         print("Done visiting")
 
 # TODO: It may be desirable to associate saved images with JSON of status for later searching
-def scrape_user_images(user_id,limit,data_dir):
+def scrape_user_images(user_id, limit, data_dir, api):
     """
-    Scrape a single user for images. 
+    Scrape a single user for images.
     Places all images in data_dir/user_id/ along with
     JSON of user object.
     Returns the number of images scraped.
@@ -107,61 +118,67 @@ def scrape_user_images(user_id,limit,data_dir):
     limit -- the maximum number of images to scrape from this user. If -1, no limit
     data_dir -- the directory to save images to
     """
-    if (DEBUG): print("Scraping user " + str(user_id))
-    
+    if DEBUG:
+        print("Scraping user " + str(user_id))
+
     user = api.get_user(user_id)
-    if (user.protected == True): return 0
+    if user.protected:
+        return 0
 
     # Create directory to store user's data and images
-    user_dir = os.path.join(data_dir,str(user_id))
+    user_dir = os.path.join(data_dir, str(user_id))
     if not os.path.exists(user_dir):
-            os.makedirs(user_dir)
+        os.makedirs(user_dir)
     else:
         return 0 #already have user data
 
     # Save user data
-    f = open(os.path.join(user_dir,str(user_id) + ".json"),'w')
+    f = open(os.path.join(user_dir,str(user_id) + ".json"), 'w')
     f.write(str(vars(user)))
     f.close()
-    
+
     # Get all tweets from user
     # TODO: Could improve speed by grabbing images inside this loop until hit max
-    tweets = api.user_timeline(id=user_id,count=200)
+    tweets = api.user_timeline(id=user_id, count=200)
     if len(tweets) is not 0:
         last_id = tweets[-1].id
     else: return 0 # no tweets to scrape
-    while (True):
-        tweets_to_append = api.user_timeline(id=user_id,count=200,
-                                        max_id=last_id-1)
-        if (len(tweets_to_append)==0):
+    while True:
+        tweets_to_append = api.user_timeline(id=user_id, count=200,
+                                             max_id=last_id-1)
+        if len(tweets_to_append) == 0:
             break
         else:
             last_id = tweets_to_append[-1].id-1
             tweets = tweets + tweets_to_append
-    
+
     # Collect a set of image URLs from the user
-    media_files = get_media_urls(tweets,limit)
-        
+    media_files = get_media_urls(tweets, limit)
+
     # Download the images
     n = 0 # num of images downloaded from user
     for media_file in media_files:
         try:
-            urllib.request.urlretrieve(media_file,
-                os.path.join(user_dir, str(user_id) + "_" + str(n) + ".jpg"))
+            urllib.request.urlretrieve(media_file, os.path.join(user_dir,
+                                                                str(user_id) +
+                                                                "_" + str(n) +
+                                                                ".jpg"))
             n = n + 1
         except urllib.error.HTTPError:
-            if(DEBUG): print("HTTPError, skipping media")
+            if DEBUG:
+                print("HTTPError, skipping media")
     return n
 
-def get_media_urls(tweets,limit):
+def get_media_urls(tweets, limit):
     cnt = 0
     media_files = set()
     for status in tweets:
-        media = status.entities.get('media',[])
-        if (len(media) > 0): # each status may have multiple
-            for i in range (0, len(media)):
-                if (media[i]['type'] == 'photo'):
+        media = status.entities.get('media', [])
+        if len(media) > 0: # each status may have multiple
+            for i in range(0, len(media)):
+                if media[i]['type'] == 'photo':
                     media_files.add(media[i]['media_url'])
                     cnt = cnt + 1
-                    if (limit != -1 and cnt >= limit): return media_files
+                    if (limit != -1 and cnt >= limit):
+                        return media_files
     return media_files
--- viper_scraper/vscraper.py
+++ viper_scraper/vscraper.py
@@ -9,18 +9,7 @@ DEFAULT_NUMBER = 2500
 DEFAULT_PER_NODE_LIMIT = 100
 DEFAULT_FOLLOWER_LIMIT = 10
 
-parser = argparse.ArgumentParser(description="Scrape data from social media",
-                                epilog="For snowball sampling, reducing per-node-limit and followers-per-node will " +
-                                "reduce locality of data collected (ie increase depth of the graph traversed " +
-                                "at the cost of increased processing time to due Twitter API rate limits.")
-
-parser.add_argument('website',choices=['twitter','instagram'],
-    help='The website to crawl for data')
-parser.add_argument('seed_user',
-    help='The seed user handle, e.g. @Twitter')
-parser.add_argument('-n','--number',type=int,default=DEFAULT_NUMBER,
-    dest='number',metavar='Number',
-    help="If data type is images, the number of images to scrape. Else the number of posts to scrape.")
+"""
 parser.add_argument('-nl','--node_limit',metavar="Per-Node-Limit",
     type=int,dest='limit_per_node',default=DEFAULT_PER_NODE_LIMIT,
     help="The number of posts/images to extract per node before moving to neighbor. Note: Decreasing "
@@ -29,11 +18,35 @@ parser.add_argument('-fl','--follower_limit',metavar="Followers-Per-Node",
     type=int,dest='limit_followers_per_node',default=DEFAULT_FOLLOWER_LIMIT,
     help="For snowball sampling, the number of neigbors to visit per node. Visit all neighbors if not listed.")
 args = parser.parse_args()
+"""
 
-if (args.website=='twitter'):
-    start_time = time.time()
-    tscraper.snowball_scrape(seed_user_screen_name=args.seed_user,number=args.number,
-                    limit_per_user=args.limit_per_node,
-                    limit_neighbors_per_node=args.limit_followers_per_node)
-    elapsed_time = time.time()
-    print("Time elapsed: " + str(elapsed_time))
+def argument_parsing():
+    """
+    Parse the arguments
+
+    Returns args
+    """
+
+    parser = argparse.ArgumentParser(description="Scrape data from social media")
+    parser.add_argument('website', choices=['twitter', 'instagram'],
+                        help='The website to crawl for data')
+    parser.add_argument('-n', '--number', type=int, default=DEFAULT_NUMBER,
+                        dest='number', metavar='Number',
+                        help="If data type is images, the number of images to "
+                        + "scrape. Else the number of posts to scrape.")
+    return parser.parse_args()
+
+def main():
+    args = argument_parsing()
+
+    if args.website == 'twitter':
+        start_time = time.time()
+        tscraper.snowball_scrape(seed_user_screen_name=args.seed_user,
+                                 number=args.number,
+                                 limit_per_user=args.limit_per_node,
+                                 limit_neighbors_per_node=args.limit_followers_per_node)
+        elapsed_time = time.time() - start_time
+        print("Time elapsed: " + str(elapsed_time))
+
+if __name__ == "__main__":
+    main()
