fbd82ad56d9b2bd8783b8cf0a20dbd371436be8c
John Alberse <alberse.john@gmail.com>
2019-02-18 14:20:02 -0600

    remove commented code

--- viper_scraper/vscraper.py
+++ viper_scraper/vscraper.py
@@ -8,17 +8,6 @@ from viper_scraper.twitter import scraper as tscraper
 
 DEFAULT_NUMBER = 2500
 
-"""
-parser.add_argument('-nl','--node_limit',metavar="Per-Node-Limit",
-    type=int,dest='limit_per_node',default=DEFAULT_PER_NODE_LIMIT,
-    help="The number of posts/images to extract per node before moving to neighbor. Note: Decreasing "
-            " this number increases the time it takes to run, due to twitter rate limits.")
-parser.add_argument('-fl','--follower_limit',metavar="Followers-Per-Node",
-    type=int,dest='limit_followers_per_node',default=DEFAULT_FOLLOWER_LIMIT,
-    help="For snowball sampling, the number of neigbors to visit per node. Visit all neighbors if not listed.")
-args = parser.parse_args()
-"""
-
 def argument_parsing():
     """
     Parse the arguments
